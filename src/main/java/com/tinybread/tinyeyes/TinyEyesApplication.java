fc1236473fb6e339ba6fe3a8e9d15bd3714f238b
LAPTOP-U29M5959\jooooo <jinju3596@naver.com>
2017-12-04 23:04:49 +0900

    init project

---
+++ src/main/java/com/tinybread/tinyeyes/TinyEyesApplication.java
@@ -0,0 +1,12 @@
+package com.tinybread.tinyeyes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinyEyesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinyEyesApplication.class, args);
+	}
+}
